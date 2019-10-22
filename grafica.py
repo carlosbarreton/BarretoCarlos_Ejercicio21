647f7b910501ade3c33c7122ab016c71a11d5024
Default user <ca.barreto@uniandes.edu.co>
2019-10-22 18:39:36 +0000

    Modificacion del codigo para que grafique la funcion coseno

--- grafica.py
+++ grafica.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,2*np.pi,100)
-y = sin(x)
+y = np.cos(x)
 
 plt.figure()
 plt.xlabel("x")
-plt.ylabel("sin(x)")
+plt.ylabel("cos(x)")
 plt.plot(x,y)
-plt.savefig("seno.png")
+plt.savefig("grafica.png")
